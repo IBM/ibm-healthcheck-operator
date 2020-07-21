cbea4910b551c0f640c70eca4da30882cac56263
Yan Meng <yanmxa@163.com>
2020-07-21 17:50:42 +0800

    delete replicaCount and swith csv 3.7.0 operator image to test registry

--- pkg/apis/operator/v1alpha1/healthservice_types.go
+++ pkg/apis/operator/v1alpha1/healthservice_types.go
@@ -30,9 +30,7 @@ type HealthServiceSpecMemcached struct {
 	Name string `json:"name,"`
 	// deprecated, define image in operator.yaml
 	Image image `json:"image,omitempty"`
-	// memcached deployment replicas, default is 0, will be deprecated
-	ReplicaCount int32 `json:"replicaCount,omitempty"`
-	// memcached pod replicas, default is 1
+	// memcached pod replicas, default is 0
 	Replicas int32 `json:"replicas,omitempty"`
 	// memcached deployment ServiceAccountName, default is default
 	ServiceAccountName string `json:"serviceAccountName,omitempty"`
@@ -62,9 +60,7 @@ type HealthServiceSpecHealthService struct {
 	ServiceNameSetting string `json:"serviceNameSetting,omitempty"`
 	// set labels/annotation name to get pod's dependencies
 	DependsSetting string `json:"dependsSetting,omitempty"`
-	// health service deployment replicas, default is 0, will be deprecated
-	ReplicaCount int32 `json:"replicaCount,omitempty"`
-	// health service pod replicas, default is 1
+	// health service pod replicas, default is 0
 	Replicas int32 `json:"replicas,omitempty"`
 	// health service deployment ServiceAccountName, default is default
 	ServiceAccountName string `json:"serviceAccountName,omitempty"`
