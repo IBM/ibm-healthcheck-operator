46ef0f48b45d935ad1286abb735d96830b5b1b0c
Qing Hao <haoqing@cn.ibm.com>
2020-08-25 18:17:39 +0800

    remove metering product version (#78)

--- pkg/controller/mustgatherservice/mustgatherservice.go
+++ pkg/controller/mustgatherservice/mustgatherservice.go
@@ -545,10 +545,9 @@ func labelsForMustGatherService(name string, releaseName string) map[string]stri
 
 func annotationsForMustGatherService() map[string]string {
 	return map[string]string{
-		"productName":    "IBM Cloud Platform Common Services",
-		"productID":      "068a62892a1e4db39641342e592daa25",
-		"productVersion": "3.4.0",
-		"productMetric":  "FREE",
+		"productName":   "IBM Cloud Platform Common Services",
+		"productID":     "068a62892a1e4db39641342e592daa25",
+		"productMetric": "FREE",
 	}
 }
 
